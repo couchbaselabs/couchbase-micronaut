a8e1f55ac2bcdd483066c1526a894b9898ae2bb9
Timoteo Soutello <tsoutello@gmail.com>
2022-11-15 08:22:08 -0300

    adjust build pipeline

--- couchbase/src/test/java/util/TestUtil.java
+++ couchbase/src/test/java/util/TestUtil.java
@@ -28,11 +28,10 @@ public class TestUtil {
 		container = new CouchbaseContainer(COUCHBASE_CONTAINER_BASE_IMAGE)
 				.withCredentials(COUCHBASE_USERNAME, COUCHBASE_PASSWORD)
 				.withExposedPorts(8091, 8092, 8093, 8094, 8095, 8096, 9100, 9101, 9102, 9103, 9104, 9105,
-						9119, 9999, 21200, 21100, 21150, 21250, 21300)
+						9119, 9999, 21200, 21100, 21150, 21250)
 				.withBucket(bucketDefinition).withStartupAttempts(2).withStartupTimeout(Duration.ofMinutes(2));
 		container.setPortBindings(Arrays.asList("8091:8091", "8092:8092", "8093:8093", "8094:8094", "8095:8095",
-				"8096:8096", "9100:9100", "9101:9101", "9102:9102", "9103:9103", "9104:9104", "9105:9105", "11210:11210", "9119:9119", "9999:9999", "21200:21200", "21250:21250",
-				"21300:21300", "21100:21100", "21150:21150"));
+				"8096:8096", "9100:9100", "9101:9101", "9102:9102", "9103:9103", "9104:9104", "9105:9105", "11210:11210", "9119:9119", "9999:9999", "21200:21200", "21250:21250", "21100:21100", "21150:21150"));
 		container.start();
 		Map<String, Object> settings = CollectionUtils.mapOf(CouchbaseSettings.PREFIX + "." + CouchbaseSettings.URI,
 				container.getConnectionString(), CouchbaseSettings.PREFIX + "." + CouchbaseSettings.USERNAME,
